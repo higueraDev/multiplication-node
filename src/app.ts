3291c6107cfa65be6a70a0df2c5d44506f9eda4b
higueradev <contact@higueradev.com>
2025-01-03 12:34:07 -0600

    feat(app): main template

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+import { mkdirSync, writeFileSync } from "fs";
+
+const base = 5;
+const header = `
+===========================
+ Multiplication table of ${base} 
+===========================
+`;
+
+let table = `${header}`;
+
+for (let i = 1; i <= 10; i++) {
+	const line = `${base} x ${i} = ${base * i}\n`;
+	table += line;
+}
+
+const filePath = "outputs";
+
+mkdirSync(filePath, { recursive: true });
+writeFileSync(`outputs/table-${base}.txt`, table);
+
+console.log(table);
